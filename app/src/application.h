9bf2b5e108a39cdb7a39666e46b3851d0caba660
Byte-Wh1te <bytewh1te@yahoo.com>
2024-02-09 23:41:25 +0200

    working on the ui

--- app/src/application.h
+++ app/src/application.h
@@ -1,9 +1,18 @@
 #include "Magma/Magma.h"
 #include "serial/serial.h"
+#include <vector>
+#include <string>
+#include <chrono>
 
 
 class FireBridgeApplication :public mg::Application
 {
+	enum class TimeMode
+	{
+		OFF = 0,
+		TimeHMS,		// H:M:S:ms
+		SinceStartup,	// in ms
+	}m_timemode = TimeMode::OFF;
 public:
 	FireBridgeApplication();
 	~FireBridgeApplication();
@@ -14,8 +23,16 @@ private:
 	void SetupTheme();
 	void CheckConfig();
 private:
+	struct MessageData {
+		std::string content;
+		std::chrono::steady_clock::time_point time;
+	};
 	std::vector<serial::PortInfo> m_ports;
 	std::vector<int> m_baudRates = { 4800, 9600, 19200, 38400, 57600, 115200 };
-	int m_currentItem = 0;
+	int m_selectedPortIndex = 0;
 	int m_selectedBaudRateIndex = 5; // start from 115200
+	bool m_newline = true;
+	char m_message[128] = "";
+	serial::Serial* m_serial = nullptr;
+	std::vector<MessageData> m_monitor;
 };
--- app/src/ui.cpp
+++ app/src/ui.cpp
@@ -1,18 +1,25 @@
 #include "application.h"
 
+#define RADIO_BUTTON_TIMEMODE(msg,mode) if (ImGui::RadioButton(msg, (m_timemode == mode)))m_timemode = mode;
+
+
+
+
 void FireBridgeApplication::Render()
 {
+    static bool connect_error = false;
+    static std::string connect_exception = "";
 	{
 		ImGui::Begin("Settings");
 		
-        if (ImGui::BeginCombo("Port COM", m_ports[m_currentItem].port.c_str()))
+        if (ImGui::BeginCombo("Port COM", m_ports[m_selectedPortIndex].port.c_str()))
         {
             for (int i = 0; i < m_ports.size(); ++i)
             {
-                const bool isSelected = (m_currentItem == i);
+                const bool isSelected = (m_selectedPortIndex == i);
                 if (ImGui::Selectable(m_ports[i].port.c_str(), isSelected))
                 {
-                    m_currentItem = i;
+                    m_selectedPortIndex = i;
                 }
                 if (isSelected)
                 {
@@ -36,17 +43,93 @@ void FireBridgeApplication::Render()
         }
         if (ImGui::Button("Refresh"))
             m_ports = serial::list_ports();
-        static bool is_on = false;
-        ImGui::RadioButton("display time [H:M:S:ms]",is_on);
-        ImGui::RadioButton("display time since the program has been started [ms]", is_on);
-        ImGui::RadioButton("OFF", is_on);
+        ImGui::SameLine();
+        if (ImGui::Button("Connect"))
+        {
+            connect_error = false;
+            connect_exception = "";
+            if (m_serial != nullptr) delete m_serial;
+            try {
 
+            m_serial = new serial::Serial(
+                m_ports[m_selectedPortIndex].port,
+                m_baudRates[m_selectedBaudRateIndex],
+                serial::Timeout::simpleTimeout(1000)
+            );
+            }
+            catch (std::exception e)
+            {
+                //delete m_serial;
+                m_serial = nullptr;
+                connect_error = true;
+                connect_exception = e.what();
+            }
+
+            if(!connect_error) 
+                if (!m_serial->isOpen())
+                {
+                    delete m_serial;
+                    m_serial = nullptr;
+                    connect_error = true;
+                }
+        }
+        RADIO_BUTTON_TIMEMODE("display time [H:M:S]", TimeMode::TimeHMS);
+        RADIO_BUTTON_TIMEMODE("display time since the program has been started [ms]", TimeMode::SinceStartup);
+        RADIO_BUTTON_TIMEMODE("OFF", TimeMode::OFF);
+        ImGui::Checkbox("New line for each read", &m_newline);
+        ImGui::Separator();
+        ImGui::InputTextMultiline("message", m_message, 128);
+        ImGui::SameLine();
+        ImGui::Button("Send");
+        ImGui::SeparatorText("Monitor");
+        if (ImGui::Button("Copy recieved to clipboard"))
+        {
+            
+        }
+        ImGui::SameLine();
+        if(ImGui::Button("Clear recieved")) m_monitor.clear();
 		ImGui::End();
 	}
 
+    if (m_serial != nullptr)
+    { // saves data if available and if connected
+        if (int buff_size = m_serial->available())
+        {
+            MessageData data;
+            data.content = m_serial->read(buff_size);
+            data.time = std::chrono::steady_clock::now();
+            m_monitor.emplace_back(data);
+        }
+    }
+
 	{
 		ImGui::Begin("Monitor");
-        
+        if (m_serial != nullptr)
+        {
+            for (auto i : m_monitor)
+            {
+                auto time_since_epoch = i.time.time_since_epoch();
+                auto seconds = std::chrono::duration_cast<std::chrono::seconds>(time_since_epoch).count();
+                auto hours = seconds / 3600;
+                auto minutes = (seconds % 3600) / 60;
+                seconds = seconds % 60;
+
+                if (m_timemode == TimeMode::TimeHMS)ImGui::TextColored(ImVec4(0.f, 1.f, 0.f, 1.f), 
+                    "[%02d:%02d:%02d]", hours,minutes,seconds
+                );
+
+                ImGui::SameLine();
+                ImGui::Text(i.content.c_str());
+                //todo: if (!m_newline)
+            }
+        }
+        else
+        {
+            if(connect_error)
+            ImGui::TextColored(ImVec4(0.8f,0.f,0.f,1.f), "Error: Could not connect.\n%s", connect_exception.c_str());
+            else
+            ImGui::TextDisabled("(not connected)");
+        }
 		ImGui::End();
 	}
 }
